204249842605fce5876fc8ac751af3ed136ed4d5
athnme <a.oezbanazi@gmail.com>
2020-10-06 22:52:09 +0200

    not true to given design but instead have taken the advice of coaches

---
+++ src/components/Logo/logo.stories.js
@@ -0,0 +1,6 @@
+import "./logo.css";
+import logo from "./logo.html";
+
+export default { title: "Components/logo" };
+
+export const Logo = () => logo;
--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,10 @@
 import "./button.css";
-import buttonText from "./button-text.html";
-import buttonLogo from "./button-logo.html";
+import buttonShare from "./button-share.html";
+import buttonFavorite from "./button-favorite.html";
+import buttonPlay from "./button-play.html";
 
 export default { title: "Components/Button" };
 
-export const withText = () => buttonText;
-export const withLogo = () => buttonLogo;
+export const Share = () => buttonShare;
+export const Fav = () => buttonFavorite;
+export const Play = () => buttonPlay;
